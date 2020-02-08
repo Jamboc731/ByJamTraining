9aba3227ed3f73266808726791a83b686f27d993
ByronCrowhurst <bc216857@falmouth.ac.uk>
2020-02-08 21:11:13 +0000

    Interactables implement

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour, IInteractable
+{
+    IInteractable.EPickedUpState ECurrentState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ECurrentState = IInteractable.EPickedUpState.putdown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Interact(Collider col)
+    {
+        switch (ECurrentState)
+        {
+            case IInteractable.EPickedUpState.putdown:
+                transform.parent = col.transform;
+                ECurrentState = IInteractable.EPickedUpState.pickedup;
+                break;
+        }
+    }
+
+    public void Deinteract()
+    {
+        switch (ECurrentState)
+        {
+            case IInteractable.EPickedUpState.pickedup:
+                transform.parent = null;
+                ECurrentState = IInteractable.EPickedUpState.putdown;
+                break;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Interfaces.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+    internal enum EPickedUpState { pickedup, putdown };
+    void Interact(Collider col);
+    void Deinteract();
+}
+
+
+//public class Interfaces : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+        
+//    }
+//} 
